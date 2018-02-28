e8017dc671a43de7cb7be5aed61655436a0e52fc
Ritsuki Sato <ric.u.k.ky@gmail.com>
2018-02-28 06:02:53 +0000

    first_commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,10 @@
+class BooksController < ApplicationController
+  def detail
+  end
+  
+  def detail2
+  end
+  
+  def detail3
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get '/detail'  , to: 'books#detail'
+  get '/detail/2', to: 'books#detail2'
+  get '/detail/3', to: 'books#detail3'
 end
