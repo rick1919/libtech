8dd06e0be5c75e1e7600cd2d0c04d138f56a0fe3
Ritsuki Sato <ric.u.k.ky@gmail.com>
2018-03-01 07:57:29 +0000

    title_author_overview

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  get '/detail'  , to: 'books#detail'
-  get '/detail/2', to: 'books#detail2'
-  get '/detail/3', to: 'books#detail3'
-  get '/detail/4', to: 'books#detail4'
-  get '/detail/5', to: 'books#detail5'
+  get 'books/detail/:id',to: 'books#detail'
+ 
 end
