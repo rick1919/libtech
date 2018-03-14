e85100ee0d4bbc98342283a6ff0e07676d9507cb
Ritsuki Sato <ric.u.k.ky@gmail.com>
2018-03-14 05:13:36 +0000

    詳細issue完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ActiveRecord::Base
-  validates :title,  presence: true, length: { maximum: 40 }
-  validates :summary,presence: true,length:{ maximum: 500}
-  validates :author,presence: true,length:{maximum: 20}
-  validates :url,presence: true,length:{maximum: 200}
+  validates :title,  presence: true, length: { maximum: 100 }
+  validates :summary,presence: true, length:{ maximum: 200}
+  validates :author, presence: true, length:{maximum: 20}
+  validates :url,    presence: true, length:{maximum: 250}
 end
