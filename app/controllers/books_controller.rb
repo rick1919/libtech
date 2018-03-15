1b99bf7cf7c05419d5ff33eca18c974279104d53
Ritsuki Sato <ric.u.k.ky@gmail.com>
2018-03-15 08:31:12 +0000

    検索ボタンの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,9 @@ class BooksController < ApplicationController
   def detail
     @book = Book.find(params[:id])
   end
+  def index
+    
+  end
+
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
   get 'books/detail/:id',to: 'books#detail'
+  #get '/index' => 'books#index'
+  
   resources :books
 end
