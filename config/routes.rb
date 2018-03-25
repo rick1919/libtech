0daaabe1329b01d69d957721de6f63286c2d37ba
Ritsuki Sato <ric.u.k.ky@gmail.com>
2018-03-25 13:23:17 +0000

    Update Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     get '/' => 'books#index'
     get 'books/:id',to: 'books#detail'
     post 'books/search' => 'books#search'
+    get '/books' => 'books#detail'
     resources :books do
   
   end
