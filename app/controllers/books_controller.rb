f50183a865694773104182e3b52fb5ea56aecb88
Ritsuki Sato <ric.u.k.ky@gmail.com>
2018-03-26 09:50:06 +0000

    エラー解決

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
   def detail
-    @book = Book.all
+    @book = Book.find(params[:id])
   end
   def index
     @book = Book.all
   end
   def search
-    @book = Book.where("title like ?", "%" + params[:title] + "%")
-    #@book = Book.where("title LIKE ?", "%#{params[:title]}%")
+    @book = Book.where("title like ?","%" + params[:title] + "%")
     render :action =>'index'
+  
   end
+  
 end
